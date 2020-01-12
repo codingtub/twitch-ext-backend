f3a067e4f1309231a049b5dfc712d97d0971080d
Michael Rieger <mr@codingtub.eu>
2020-01-12 21:08:55 +0100

    update

--- src/click/controller/click.controller.ts
+++ src/click/controller/click.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ClickData, ClickService } from '../service/click.service';
 
 @Controller('click')
